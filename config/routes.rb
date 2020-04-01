472def5e4d31a5110342d16e96defa7c35ed1f87
Sam Moore <swmoore84@gmail.com>
2020-04-01 23:12:05 +0900

    updated root in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
   resources :prefectures, only: [:index, :show, :edit, :update] do
 
   resources :visits, only: [:new, :create, :edit, :update, :destroy], shallow: true
 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'prefectures#index'
 end
